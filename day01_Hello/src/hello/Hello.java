f20227d75e7d321fa21f4b2ae57fccd2d1a838c9
Parksangyong <password1003@naver.com>
2019-07-04 11:13:14 +0900

    sysout 구문 사용법 소개

--- day01_Hello/src/hello/Hello.java
+++ day01_Hello/src/hello/Hello.java
@@ -8,7 +8,21 @@ public class Hello {
 		
 		// 자바는 대소문자가 틀리면 구문오류인 경우가 많으므로
 		// 대소문자 오타주의!
-		System.out.println("Hello, World!");	
+		System.out.println("Hello, World!");
+		// 실행 : ctrl + F11
+		// 프로잭트에 ? 표시  => 아직 add안됨 프로젝트 오른쪽클릭
+		// 메뉴 > 팀 > add to index
+		
+		// 자바 기본 출력 구문
+		// sysout => ctrl + space bar
+		System.out.println("안녕하세요, 자바!");
+		
+		// 소스편집 후에는 ctrl + s 저장 항상 저장하는 습관
+		// ctrl + (shift + 3)# 
+		
+		
+		
+		
 	}
 
 }

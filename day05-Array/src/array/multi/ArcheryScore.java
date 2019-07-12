6ecadfbfce48f9a25a8cb3555785f8ca1a6479d1
Parksangyong <password1003@naver.com>
2019-07-12 16:40:16 +0900

    배열 궁수점수문제

---
+++ day05-Array/src/array/multi/ArcheryScore.java
@@ -0,0 +1,28 @@
+package array.multi;
+
+import java.util.Scanner;
+
+/**
+ * 3명의 양궁선수가 맞힌 과녁의 점수를 각각 차시(9차시까지) 별로 배열에 입력하고
+ * 전체 합산 점수가 가장 높은 선수의 인덱스와 점수를 출력
+ * @author Administrator
+ *
+ */
+public class ArcheryScore {
+
+	public static void main(String[] args) {
+		// 1. 선언, 2. 초기화
+		Scanner scan = new Scanner(System.in);
+		int[][] score;
+		score = new int [3][9];
+		int [] sum;
+		sum = new int[3];
+		int max = 0;
+		
+		// 각 선수별로 과녁 점수를 입력받아 배열에 저장
+		System.out.println("양궁 선수 점수 입력");
+		
+
+	}
+
+}

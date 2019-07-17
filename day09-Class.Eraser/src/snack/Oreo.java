b9412ced10cc944387bda3c39898c3d8721a777c
Parksangyong <password1003@naver.com>
2019-07-17 15:45:55 +0900

    오레오 쿠키 테스트

---
+++ day09-Class.Eraser/src/snack/Oreo.java
@@ -0,0 +1,66 @@
+package snack;
+
+/**
+ * Oreo 쿠키를 정의하는 클래스
+ * -----------------------------
+ * 속성 :
+ * cream     : String : 바닐라, 초코, 딸기, 말차
+ * radius    : double : 오레오 쿠키 1개의 반지름 길이
+ * thickness : double : 오레오 쿠키 1개의 두께
+ * -------------------------------------------
+ * 생성자 : 기본생성자, 중복된 this, this() 로 정의하는 생성자
+ * 총 4개의 생성자 중복정의
+ * -------------------------------------------
+ * 메소드 :
+ * print() : void : 오레오 쿠키의 상태출력
+ * changeCream(String cream) : void : 매개변수로 입력된 cream 값으로 오레오 쿠키의 크림을 변경함
+ * --------------------------------------------------
+ *   
+ * @author Administrator
+ *
+ */
+public class Oreo {
+	// 1. 변수선언부
+	String cream;
+	double radius;
+	double thickness;
+	
+	// 2. 생성자 선언부
+	// 2.1 기본 생성자 (CLASS(Oreo)와 이름이 같고 매개변수가 비어있음
+	Oreo(){
+		
+	}
+	
+	// 2.2 this() 생성자
+	Oreo(String cream){
+		this();
+		this.cream = cream;
+	}
+	
+	// 2.3 this()에2개를 받는 생성자
+	Oreo(String cream, double radius){
+		this(cream);
+		this.radius = radius;
+	}
+	
+	// 2.3 this()에3개를 받는 생성자
+	Oreo(String cream, double radius, double thickness){
+		this(cream, radius);
+		this.thickness = thickness;
+	}
+		
+	
+	// 3. 메소드 선언부
+	public void changCream(String cream) {
+		this.cream = cream;
+	}
+	
+	public void print() {
+		System.out.printf("현재 오레오 상태는 크림%s색 반지름%4.1f 두께%4.1f 입니다.", cream, radius, thickness);
+	}
+	
+	
+	
+	
+	
+}
---
+++ day09-Class.Eraser/src/snack/OreoTest.java
@@ -0,0 +1,26 @@
+package snack;
+
+/**
+ * Oreo 쿠키가 3개 들어있는 oreoBox 라는 변수를
+ * Oreo 배열타입으로 선언하고
+ * 
+ * oreoBox[0] ~ [2] Oreo 쿠키를 입력
+ * 크림의 종류는 바닐라, 초코, 딸기, 말차 중 하나로 입력할 것. 
+ * 
+ * foreach를 사용하여
+ * oreoBox 에서 하나의 쿠키의 상태를 출력
+ * 
+ * for 를 사용하여 모든 쿠키의 크림을 바닐라로 변경
+ * 
+ * foreach를 사용해서 모든 쿠키의 크림 변경 상태 확인을 위한 출력
+ * @author Administrator
+ *
+ */
+public class OreoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

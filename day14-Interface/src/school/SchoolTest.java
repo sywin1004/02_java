eabb1ad9e3c27d4591bda90a7afa2171748c35bd
Parksangyong <password1003@naver.com>
2019-07-24 20:21:34 +0900

    스쿨테스트

--- day14-Interface/src/school/SchoolTest.java
+++ day14-Interface/src/school/SchoolTest.java
@@ -13,16 +13,10 @@ public class SchoolTest {
 
 	public static void main(String[] args) {
 		// 1. 선언 
-		// Person 타입의 배열 persons를 선언
+		// Attendance 타입의 배열 attends를 선언
 		Attendance[] attends; //= {new Student("S01", "홍길동", 18, "도술"), new Teacher("T01", "정약용", 32, "실학"), new Employee("E01", "허균", 48, "문학예술지원부")};
 		
 		// 2. 초기화
-		// (1) persons 를 3 사이즈로 생성
-		// (2) persons 배열의 각각 0 ~ 2 인덱스에
-		// [0] : Student : id=S01, name=홍길동, age=18, major=도술
-		// [1] : Teacher : id=T01, name=정약용, age=32, subject=실학
-		// [2] : Employee : id=E01, name=허균, age=48, dept=문학예술지원부
-		// 상위타입의 변수 Person에 하위타입 인스턴스(멤버변수)를 사용할 수 있는 이유: Student 상속에의한 다형성제공 에 의한 형상
 		attends = new Attendance[3];
 		attends[0] = new Student("S01", "홍길동", 18, "도술");
 		attends[1] = new Teacher("T01", "정약용", 32, "실학");
@@ -31,7 +25,27 @@ public class SchoolTest {
 		// 실행
 		// foreach를 사용하여 persons 배열의 각 엘리먼트에 대하여
 		// print() 메소드 호출
-		for(Attendance person: attends) {
+		for(Attendance attend: attends) {
+			// 가상 메소드 호출(virtual method invocation)
+			System.out.println(attend.attend());
+		}
+		
+		/////////////////////////////////////////
+		Lesson[] lessons = new Lesson[2];
+		lessons[0] = new Student("S01", "홍길동", 18, "도술");
+		lessons[1] = new Teacher("T01", "정약용", 32, "실학");
+		
+		for (Lesson lesson: lessons) {
+			System.out.println(lesson.lesson());
+		}
+		
+		Person[] persons;
+		persons = new Person[3];
+		persons[0] = new Student("S01", "홍길동", 18, "도술");
+		persons[1] = new Teacher("T01", "정약용", 32, "실학");
+		persons[2] = new Employee("E01", "허균", 48, "문학예술지원부");		
+		
+		for(Person person: persons) {
 			System.out.println(person.attend());
 		}
 	}

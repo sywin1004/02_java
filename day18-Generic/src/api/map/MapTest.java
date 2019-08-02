d800aae226367873fee9dfe2dc95f718fbab2a30
Parksangyong <password1003@naver.com>
2019-08-02 14:11:33 +0900

    api Map Key Value

---
+++ day18-Generic/src/api/map/MapTest.java
@@ -0,0 +1,28 @@
+package api.map;
+
+/**
+ * 데이터를 Key, Value 를 쌍으로 저장하는 자료구조인
+ * Map 을 테스트하는 클래스
+ * 
+ * Key : 중복을 허용하지 않음 순서는 상관없음(Set의 특징을 가짐)
+ * Value : 중복을 허용
+ * 
+ * Key, Value 는 반드시 한 쌍으로만 취급할 수 있음
+ * Key 없는 Value 를 만들 수 없음.
+ * Key 없이 Value 를 찾을 수 없음
+ * 
+ * Key, Value 에 모든 (참조)타입의 데이터 저장 가능
+ * 
+ * --------------------------------------------
+ * 
+ * @author Administrator
+ *
+ */
+public class MapTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

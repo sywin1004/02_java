2303477a0c26efe84aed4e826d7d3cb67a9f247c
Parksangyong <password1003@naver.com>
2019-08-19 10:56:21 +0900

    Emp Mapper interface 동적SQL

--- day28-Mybatis.Interface/src/mybatis/emp/test/MybatisEmpSelectTest4.java
+++ day28-Mybatis.Interface/src/mybatis/emp/test/MybatisEmpSelectTest4.java
@@ -13,7 +13,8 @@ import mybatis.emp.vo.Emp;
 /**
  * EMP 테이블에서 1건, 전체조회를
  * Mybatis 를 사용하여 실행하는 테스트 클래스
- * @author Administrator
+ * 
+ * @author 304
  *
  */
 public class MybatisEmpSelectTest4 {
@@ -21,68 +22,46 @@ public class MybatisEmpSelectTest4 {
 	public static void main(String[] args) {
 		// 1. factory 얻기
 		SqlSessionFactory factory = MybatisClient.getFactory();
+		
 		// 2. 세션 얻기
 		SqlSession session = factory.openSession();
 		
-		
 		try {
-			// 3. 세션을 통해 쿼리 실행
-			// 매퍼 인터페이스의 메소드를 사용한 쿼리 수행
-			// (1) 전체 조회 mybatis.emp.mapper.EmpMapper.selectAll
-			
 			// 3. 매퍼 인터페이스의 메소드를 사용한 쿼리 수행
 			// (1) EmpMapper 타입의 인스턴스 얻기
 			EmpMapper mapper = session.getMapper(EmpMapper.class);
-			List<Map<String, Object>> emps2 = mapper.selectAll();
-			List<Emp> emps = mapper.selectAll2();
 			
-			// 조회에 사용할 객체
+			// 조회에 사용할 객체 생성
 			Emp inputEmp = new Emp();
 			inputEmp.setEmpno(7698); // BLAKE 조회
 			
-			System.out.println(mapper.select(inputEmp));
-			// 조회 결과 foreach 출력
-			for (Emp emp: emps) {
-				// emp 클래스 객체출력
-				System.out.println(emp);
-			}
+			// (2) 동적 쿼리 메소드 호출 & 출력
+//			System.out.println(mapper.select(inputEmp));
 			
-			System.out.println("===============================================");
-			
-			
-			// (2) 직원 1명 조회 mybatis.emp.mapper.EmpMapper.selectEmp
-			// 조회를위한 객체 생성 : empno가 들어있는 emp타입의 객체
-			
-			System.out.println("직원 사번으로 조회");
-			for(Emp emp: mapper.select(inputEmp)) {
-				System.out.println(emp);
-			}
-			
-			System.out.println("이름에 J가 들어가는 직원 조회");
-			for(Emp emp: mapper.selectByName("J")) {
+			System.out.println("== 직원 사번으로 조회 ==");
+			for (Emp emp: mapper.select(inputEmp)) {
 				System.out.println(emp);
 			}
 			
 			
+			System.out.println("== 이름에 J가 들어가는 직원 조회==");
+			for (Emp emp: mapper.selectByName("J")) {
+				System.out.println(emp);
+			}
 			
-			Emp inputEmp = new Emp();
-			inputEmp.setEmpno(8888);
-//			Emp emp = session.selectOne("mybatis.emp.mapper.EmpMapper.selectEmp2", inputEmp); // Mybatis가 inputEmp.get을 바로 불러줌
+			System.out.println("== 이름에 J가 들어가는 직원 조회시 null 전달==");
+			for (Emp emp: mapper.selectByName(null)) {
+				System.out.println(emp);
+			}
 			
-			// 직원 1명 결과 출력
-			System.out.println(emp);
+			System.out.println("=========================================");
 			
 		} finally {
 			// 4. 닫기
-			if(session != null) {
+			if (session != null)
 				session.close();
-			}
 		}
 		
-		
-		// 4. 닫기
-		
-
 	}
 
 }
--- day28-Mybatis.Interface/src/mybatis/emp/test/MybatisEmpUpdateTest3.java
+++ day28-Mybatis.Interface/src/mybatis/emp/test/MybatisEmpUpdateTest3.java
@@ -8,90 +8,90 @@ import mybatis.emp.mapper.EmpMapper;
 import mybatis.emp.vo.Emp;
 
 /**
- * EMP 테이블에서 직원정보 1건을 수정하는
+ * EMP 테이블에서 직원 정보 1건을 수정하는
  * 쿼리를 Mybatis 로 실행하는 테스트 클래스
  * -----------------------------------------
- * 수정 : 선 조회 => 후 수정
+ * 수정 : 선 조회 -> 후 수정
  * 
  * 1. 직원 사번으로 1명의 정보를 조회
  * 2. 조회된 내용이 맵으로 전달
  * 3. 2의 맵에 수정할 키(컬럼명)만 새로 put
- * 4. 3에서 변경된 맵 객체로 업데이트 실행 
- * @author Administrator
+ * 4. 3에서 변경된 맵 객체로 update 실행
+ * 
+ * @author 304
  *
  */
 public class MybatisEmpUpdateTest3 {
 
 	public static void main(String[] args) {
-		// 1. factory 얻기
+		// 1. 팩토리 얻기
 		SqlSessionFactory factory = MybatisClient.getFactory();
 		
 		// 2. 세션 얻기
 		SqlSession session = factory.openSession(true);
 		
-		// 3. 세션에서 매퍼 인터페이스 객체 얻어서 메소드 호출
 		try {
-			// (1) 직원 1명 정보 조회
-			// selelctEmp(emp)
-			// mybatis.emp.mapper.EmpMapper.selectEmp
-
-			// (1) 매퍼 인터페이스 변수 선언
-			EmpMapper mapper;
-			mapper = session.getMapper(EmpMapper.class);
-			// (2) 인터페이스 변수에 매퍼 객체 얻기
-			
-			// 한건 조회 메소드 호출
-			Emp emp = mapper.selectEmp2(emp);
-			
-			// (3) 조회
+			// 조회에 사용할 객체 선언
 			Emp inputEmp = new Emp();
 			inputEmp.setEmpno(1111);
-//			Emp emp = session.selectOne("mybatis.emp.mapper.EmpMapper.selectEmp2", inputEmp);
 			
+			// 3. 세션에서 매퍼 인터페이스 객체 얻어서 메소드 호출
+			// (1) 매퍼 인터페이스 변수 선언
+			EmpMapper mapper;
+			// (2) 인터페이스 변수에 매퍼 객체 얻기
+			mapper = session.getMapper(EmpMapper.class);
+			
+			// (3) 한건 조회 메소드 호출
+			Emp emp = mapper.selectEmp2(inputEmp);
+			
+//			Emp emp =
+//					session.selectOne("mybatis.emp.mapper."
+//							+ "EmpMapper.selectEmp2", inputEmp);
 			
 			// 맵 객체인 emp 에 담긴 내용
-			// 1111 박준석 SALESMAN
-			System.out.println(emp);
+			// 1111	박준석	SALESMAN		19/08/10
 			
-			//수정할 컬럼=값
-			// mgr = 7698, sal=1260, comm=400, deptno=30
-			emp.setMgr(7698);
-			emp.setSal(1260);
-			emp.setComm(400);
+			// 수정할 컬럼=값
+			// mgr=7698, sal=1250, comm=400, deptno=30
+			emp.setMgr(7698); 
+			emp.setSal(1250);
+			emp.setComm(800);
 			emp.setDeptno(30);
 			
-
-			// (2) 조회된 직원 정보 일부 수정
-			// mybatis.emp.mapper.EmpMapper.update
-//			int setCnt = session.update("mybatis.emp.mapper.EmpMapper.update2", emp);
-			int setCnt;
-			setCnt = mapper.update2(emp);
+			// (4) 조회된 직원 정보 일부 수정메소드 호출
+			//     update2(emp)호출
+			int setCnt = mapper.update2(emp);
 			
-			if(setCnt > 0) {
-				System.out.printf("%d 직원의 정보가 %d건 수정되었습니다%n", emp.getEmpno(), setCnt);
+//			int setCnt = 
+//					session.update("mybatis.emp.mapper.EmpMapper.update2"
+//							      , emp);
+			
+			if (setCnt > 0) {				
+				System.out.printf("%d 직원의 정보가 %d건 수정되었습니다.%n"
+						         , emp.getEmpno() , setCnt);
 			}
 			
-			// (3) 수정된 직원 정보 재 조회
-
-			Emp newEmp = new Emp();
-			newEmp.setEmpno(1111);
-			Emp resultEmp; 
-//			= session.selectOne("mybatis.emp.mapper.EmpMapper.selectEmp2", newEmp);
+			// (5) 수정된 정보 재 조회
+			Emp selectEmp = new Emp();
+			selectEmp.setEmpno(1111);
 			
 			// 매퍼에 selectEmp2(emp) 호출
+			Emp newEmp = mapper.selectEmp2(selectEmp);
+			
+//			Emp newEmp =
+//					session.selectOne("mybatis.emp.mapper"
+//							+ ".EmpMapper.selectEmp2", selectEmp);
 			
 			System.out.println("수정된 정보");
-			System.out.println(resultEmp);
+			System.out.println(newEmp);
 			
 		} finally {
 			// 4. 세션 닫기
-			if(session != null) {
+			if (session != null) {
 				session.close();
 			}
 		}
-		
-		
-
 	}
 
 }
+

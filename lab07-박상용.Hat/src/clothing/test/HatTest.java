0e5a9f7c4ce6cdbcf2a66d85779f2fce316a0dff
Parksangyong <password1003@naver.com>
2019-08-19 19:59:44 +0900

    Hat 클래스

--- lab07-박상용.Hat/src/clothing/test/HatTest.java
+++ lab07-박상용.Hat/src/clothing/test/HatTest.java
@@ -1,5 +1,8 @@
 package clothing.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import clothing.Hat;
 
 /**
@@ -10,12 +13,27 @@ import clothing.Hat;
 public class HatTest {
 
 	public static void main(String[] args) {
-		// 선언
-		Hat cap = new Hat("Ht001", "야구모자", "가죽", 15, "주황색", 35000, 8, 'M');
+		// 선언 초기화
+		/**
+		 * 야구모자를 정보를 입력하여 선언 및 초기화
+		 */
+		Hat cap = new Hat("HT001", "야구모자", "가죽", 15, "주황색", 12000, 8, 'M');
+		Hat fedora = new Hat("HT002", "페도라", "천", 10, "빨강색", 15000, 15, 'F');
+		Hat sunCap = new Hat("HT003", "썬캡", "플라스틱", 20, "검정색", 10000, 17, 'U');
 		
-		// 초기화
+		/**
+		 * 야구모자들을 담는 List<Hat> 배열 생성 후 모자들 담기
+		 */
+		List<Hat> hats = new ArrayList();
+		hats.add(cap);
+		hats.add(fedora);
+		hats.add(sunCap);
 		
 		// 사용
+		for (Hat hat: hats) {
+			System.out.println(hat);
+		}
+		
 
 	}
 

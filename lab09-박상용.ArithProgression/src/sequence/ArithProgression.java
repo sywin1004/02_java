393311e57a2e442ef0007f73f01578fc56fc2010
Parksangyong <password1003@naver.com>
2019-08-21 20:02:01 +0900

    등차수열 클래스

--- lab09-박상용.ArithProgression/src/sequence/ArithProgression.java
+++ lab09-박상용.ArithProgression/src/sequence/ArithProgression.java
@@ -1,43 +1,21 @@
 package sequence;
 
 /**
- * 공차 수열의 메소드를 정의하는 클래스
+ * 등차 수열의 합 메소드를 정의하는 클래스
  * @author Administrator
  *
  */
 
-/**
- * 프로젝트명 : lab09-이름.ArithProgression
-패키지명 : sequence
----------------------------------------------
-
-첫항 : start
-공차 : diff
-항의 갯수 : index 
---------------------------------------------------------
-첫 항이 start 이고, 공차가 diff 이며, 항의 갯수 index 만큼인
-등차수열의 합을 구하는 메소드 sequence 를 작성하시오.
-
-등차 수열의 일반항은
-start + diff * (index - 1) 로 구한다.
---------------------------------------------------------
-+ArithProgression     : class 
-+sequence(int start, int diff, int index) : int
---------------------------------------------------------
-+ArithProgressionTest : test class
-+main()
-start : 1, diff : 3, index : 3 ==> 12
-
- * @author Administrator
- *
- */
 public class ArithProgression {
 	public int sequence(int start, int diff, int index) {
+		int sumResult = start;
 		int result = start;
 		
-		for(int idx = )
-		result = result + diff;
+		for(int idx = 0; idx < index - 1; idx++) {
+			result += diff;
+			sumResult += result;
+		}
 		
-		return result;
+		return sumResult;
 	}
 }
--- lab09-박상용.ArithProgression/src/sequence/test/ArithProgreesionTest.java
+++ lab09-박상용.ArithProgression/src/sequence/test/ArithProgreesionTest.java
@@ -1,9 +1,24 @@
 package sequence.test;
 
+import sequence.ArithProgression;
+
+/**
+ * 등차 수열의 합을 테스트 하는 클래스
+ * @author Administrator
+ *
+ */
 public class ArithProgreesionTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArithProgression progression = new ArithProgression();
+		int result;
+		int firstInput = 1;
+		int secondInput = 3;
+		int thirdInput = 3;
+		
+		result = progression.sequence(firstInput, secondInput, thirdInput);
+		
+		System.out.printf("첫항=%d 공차=%d 항의갯수=%d 등차수열 합=%d%n", firstInput, secondInput, thirdInput, result);
 
 	}
 

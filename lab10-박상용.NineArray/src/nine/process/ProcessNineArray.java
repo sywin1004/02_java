fa3d37911115753b4ea267b8a20164255054314d
Parksangyong <password1003@naver.com>
2019-08-22 20:50:48 +0900

    고인물 구구단

---
+++ lab10-박상용.NineArray/src/nine/process/ProcessNineArray.java
@@ -0,0 +1,20 @@
+package nine.process;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 구구단을 자바고인물같이 출력하는 메소드를 정의하는 클래스
+ * @author Administrator
+ *
+ */
+public class ProcessNineArray {
+	
+	public void processNineArray(List<String> input) {
+		List<String> nineArray = new ArrayList<>();
+		
+		
+		
+	}
+	
+}
---
+++ lab10-박상용.NineArray/src/nine/process/test/ProcessNineArrayTest.java
@@ -0,0 +1,15 @@
+package nine.process.test;
+
+/**
+ *고인물 구구단을 테스트 하는 클래스
+ * @author Administrator
+ *
+ */
+public class ProcessNineArrayTest {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
